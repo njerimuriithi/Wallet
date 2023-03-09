adfbb4b6a88f802f3a5b79bbf6a134f1d6880e5d
Njeri Muriithi <carolynenjeri@cim-pool.ch>
2023-03-09 19:57:15 +0300

    Creation of Cards

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import './home.scss';
+import './home.scss';
 import { Button } from 'devextreme-react';
 import { useNavigate } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
@@ -18,15 +18,60 @@ export default function Home() {
           <Button text="Monthly"/>
         </div>
         </div>
-        <Button>
-          <h2>Total Income</h2>
-          <h3>10000</h3>
-        </Button>
-        <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-     </Card.Body>
-    </Card>
+       <div>
+       <ul className="cards">
+  <li>
+    <a href="" className="card">
+          <div className="card__overlay">
+        <div className="card__header">
+          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <img className="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+       
+          <div className="card__header-text">
+            <h3 className="card__title">Total Income</h3>            
+            <span className="card__status">1000</span>
+          </div>
+        </div>
+          </div>
+    </a>      
+  </li>
+  <li>
+    <a href="" className="card">
+        <div className="card__overlay">        
+        <div className="card__header">
+          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <img className="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+          <div className="card__header-text">
+            <h3 className="card__title">kim Cattrall</h3>
+            <span className="card__status">3 hours ago</span>
+          </div>
+        </div>
+        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+      </div>
+    </a>
+  </li>
+  <li>
+    <a href="" className="card">
+          <div className="card__overlay">
+        <div className="card__header">
+          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <img className="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+          <div className="card__header-text">
+            <h3 className="card__title">Jessica Parker</h3>
+            <span className="card__tagline">Lorem ipsum dolor sit amet consectetur</span>            
+            <span claclassNamess="card__status">1 hour ago</span>
+          </div>
+        </div>
+        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+      </div>
+    </a>
+  </li>
+  <li>
+
+  </li>    
+</ul>
+</div>
+    
+        
     </React.Fragment>
 )}
